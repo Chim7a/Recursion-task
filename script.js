ebb6107ac4f85ab783b81b48ca9d5e231a380ce0
chim7a <chimalawrence11@gmail.com>
2024-10-23 22:44:03 +0100

    Weather Adviser

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 // Ticket Pricing: Write a program that prompts the
 // user to enter their age and then determines the price
 // of a movie ticket based on the following criteria:
+
+// Prompt message to get users age.
 let getAge = prompt("Please Enter Age For Ticket Fee.");
+
+// *******
+
+// Ticket class Object.
 class Ticketing {
   constructor(age) {
     this.age = age;
   }
 
+  // Function to check condition for users age.
   getUsersAge() {
     if (this.age <= 12) {
-      alert("$10");
+      console.log("$10");
     } else if (this.age <= 17) {
-      alert("$15");
+      console.log("$15");
     } else if (this.age >= 18) {
-      alert("$20");
+      console.log("$20");
     } else {
-      alert("Please enter a valid age");
+      console.log("Please enter a valid age");
+    }
+  }
+}
+// Instance of the Ticketing class
+const printTicket = new Ticketing(getAge);
+// Calling objects and its function.
+printTicket.getUsersAge();
+
+/*Weather Clothing Adviser: Develop a program that asks 
+the user for the current temperature and whether it is raining or not. 
+Based on this information, advise the user on what clothing to wear. */
+
+class WeatherAdviser {
+  constructor(currentTemperature) {
+    this.currentTemperature = currentTemperature;
+  }
+
+  getWeatherAdvise() {
+    switch (true) {
+      case this.currentTemperature >= 90:
+        return console.log("A");
+      case this.currentTemperature >= 80:
+        return console.log("B");
+      case this.currentTemperature >= 70:
+        return console.log("C");
+      case this.currentTemperature >= 60:
+        return console.log("D");
+      case this.currentTemperature <= 59:
+        return console.log("F");
     }
   }
 }
 
-const printTicket = new Ticketing(getAge);
+const weatherAdviser = new WeatherAdviser(89);
 
-printTicket.getUsersAge();
+weatherAdviser.getWeatherAdvise();
